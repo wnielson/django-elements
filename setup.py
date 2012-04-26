b4e376b715b01ff5386d516fd1c425adc6917dae
wnielson <wnielson@gmail.com>
2012-04-25 20:42:00 -0700

    Updated setup.py and added requirements file for example project.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+import elements
+
+setup(
+    name="django-elements",
+    version=elements.__version__,
+    license="MIT",
+    url="https://github.com/wnielson/django-elements",
+    author="Weston Nielson",
+    author_email="wnielson@github",
+    description="Enhanced Markdown editing for Django projects.",
+    keywords="django markdown macros",
+    packages=[
+        "elements",
+        "elements.extensions"
+    ],
+    include_package_data = True,
+    package_data={
+        "elements": [
+            "static/elements/markitup/markdown/*.js",
+            "static/elements/markitup/markdown/*.css",
+            "static/elements/markitup/markdown/images/*.png",
+            "templates/elements/*.html"
+        ]
+    },
+    zip_safe=False,
+    install_requires=[
+        "markdown",
+        "markdown-macros"
+    ]
+)
